bc77dcc798e2ad0e2617e5e228160ac1760aafb4
Dybreak0 <JeremyJamesESangutan@bpoc.co.jp>
2021-02-17 15:32:23 +0800

    add theme support

--- wp-content/themes/template/functions.php
+++ wp-content/themes/template/functions.php
@@ -6,4 +6,13 @@ function enqueue_styles()
     wp_enqueue_script('theme-js', get_template_directory_uri() . '/public/uglified/all.js', array(), '', true);
 }
 add_action('wp_enqueue_scripts', 'enqueue_styles');
+
+function theme_supports() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('html5', array('search-form'));
+    add_theme_support('custom-logo');
+}
+add_action('after_setup_theme', 'theme_supports');
 ?>
+
--- wp-content/themes/template/header.php
+++ wp-content/themes/template/header.php
@@ -5,7 +5,6 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
 	<?php wp_head(); ?>
 </head>
 
